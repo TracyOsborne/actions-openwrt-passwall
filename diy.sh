ba8de09556fc67e7bdec419d14f950743480c1ee
zxcnk520 <42270669+zxcnk520@users.noreply.github.com>
2020-02-29 09:00:15 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+git clone https://github.com/user1121114685/koolproxyR
+./scripts/feeds update -a
+./scripts/feeds install -a
